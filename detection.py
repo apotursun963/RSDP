8dd58b57602c07b105ed93a42875165f8e18145d
abdullahTursun <124070221+apotursun963@users.noreply.github.com>
2024-10-27 13:46:23 +0300

    Add files via upload

---
+++ detection.py
@@ -0,0 +1,43 @@
+
+from keras._tf_keras.keras.models import load_model
+import numpy as nmp
+import cv2
+
+model = load_model("C:\\Users\\90507\\OneDrive\\Masaüstü\\Coding\\AI\\RSDP\\CNN-MODEL.h5")
+
+def process_image(image):
+    img = cv2.resize(image, (32,32))
+    img = img / 255.0
+    img = nmp.expand_dims(img, axis=0)
+    return (img)
+
+threshold = 0.5
+vampirt_vid = "C:\\Users\\90507\\OneDrive\\Masaüstü\\Coding\\AI\\RSDP\\vid\\RS.mp4"
+not_vampir_vid = "C:\\Users\\90507\\OneDrive\\Masaüstü\\Coding\\AI\\RSDP\\vid\\!RS.mp4"
+open_cmr = cv2.VideoCapture(not_vampir_vid)
+
+while True:
+    ret, frame = open_cmr.read()       
+
+    process_frame = process_image(frame)
+
+    tahmin = model.predict(process_frame)
+
+    label = nmp.argmax(tahmin, axis=1)
+    confidence = nmp.max(tahmin)
+    predicted_class = tahmin[label[0]]
+
+    cv2.putText(frame, f'Label: {predicted_class} Confidence: {confidence:.2f}', 
+                (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+    if predicted_class > threshold:
+        cv2.putText(frame, 'ilaci sik',
+                    (10, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+
+    cv2.imshow('Frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+open_cmr.release()
+cv2.destroyAllWindows()
